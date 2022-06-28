7f2ca2f1493315fe3ac519e5a91171eb4e5988bc
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-06-28 14:37:18 +0900

    model,migrate

---
+++ database/migrations/2022_06_28_110809_create_words_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('word_en', 255);
+            $table->string('word_ja', 255);
+            $table->integer('part_of_speech');
+            $table->integer('comprehension');
+            $table->text('memo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
