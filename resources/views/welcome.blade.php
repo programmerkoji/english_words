ab677190cdec26b8c9abf89535b9205c6e35c6ef
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-03-02 20:59:56 +0900

    welcomeページの修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,16 +24,16 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 items-center p-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/words') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">単語アプリへ</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログインはこちら</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">会員登録</a>
                         @endif
                     @endauth
                 </div>
@@ -43,10 +43,6 @@
                 <h2>ようこそ！英単語管理アプリへ！</h2>
                 <p>なかなか覚えられない英単語をアプリに登録</p>
                 <p>記憶度を × から ○ になるまで何度も見直しましょう！</p>
-                <ul class="flex">
-                    <li><a href="">ログインはこちら</a></li>
-                    <li><a href="">会員登録はこちら</a></li>
-                </ul>
             </div>
         </div>
     </body>
